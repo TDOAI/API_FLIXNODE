a0320ae89361eb26ddee236fb74f8af4cdff0438
unknown <C:\Users\Admin\AppData\Roaming\The Bat!>
2022-11-13 01:10:54 -0500

    13/11/22-1:10AM

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const compression = require("compression");
+const NodeCache = require( "node-cache" );
+const myCache = new NodeCache( { stdTTL: 100, checkperiod: 600 } );
 
 
 const slider = require('./slider')
@@ -13,8 +15,16 @@ app.get("/", (req, res) => {
 });
 
 app.get("/slider", async(req, res) => {
-  const message = await slider.main()
-  res.json(message);
+  value = myCache.get( "slider" );
+  if ( value == undefined ){
+    const message = await slider.main()
+    myCache.set( "slider", message, 3600 );
+    res.json(message);
+  }
+  else {
+    const message = myCache.get( "slider" );
+    res.json(message);
+  }
 });
 
 app.listen(5000, () => {
